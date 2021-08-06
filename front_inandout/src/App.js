ea36f02d4a07b8bfded6373e3d2e117833dadf12
jaes <dlwo9503@naver.com>
2021-08-06 16:42:28 +0900

    test

--- front_inandout/src/App.js
+++ front_inandout/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-const App = () => {
+function App() {
   return (
-    <div>
-       test11212233zxczxc
+    <div className="App">
     </div>
   );
-};
+}
 
 export default App;
