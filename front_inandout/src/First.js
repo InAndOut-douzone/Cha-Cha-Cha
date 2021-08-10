3c662038401ca3461ae0bc9be0e33458ed8dc18c
jaes <dlwo9503@naver.com>
2021-08-10 15:58:17 +0900

    First screen test

--- front_inandout/src/First.js
+++ front_inandout/src/First.js
@@ -1,33 +1,17 @@
 import React from 'react';
-import { Layout, Menu, Checkbox, Divider } from 'antd';
+import { Layout, Menu, Checkbox} from 'antd';
 import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
-const CheckboxGroup = Checkbox.Group;
-
-const plainOptions = ['내 일정', '휴가', '출장', '외근'];
-const defaultCheckedList = ['내 일정'];
-
+function onChange(e) {
+  console.log(`checked = ${e.target.checked}`);
+}
 export default function First() {
-  const [checkedList, setCheckedList] = React.useState(defaultCheckedList);
-  const [indeterminate, setIndeterminate] = React.useState(true);
-  const [checkAll, setCheckAll] = React.useState(false);
-
-  const onChange = list => {
-    setCheckedList(list);
-    setIndeterminate(!!list.length && list.length < plainOptions.length);
-    setCheckAll(list.length === plainOptions.length);
-  };
-
-  const onCheckAllChange = e => {
-    setCheckedList(e.target.checked ? plainOptions : []);
-    setIndeterminate(false);
-    setCheckAll(e.target.checked);
-  };
-
+  
   return (
+    
     <Layout>
     <Header className="header">
       <div className="logo" />
@@ -36,6 +20,7 @@ export default function First() {
         {/* <Menu.Item key="2">nav 2</Menu.Item>
         <Menu.Item key="3">nav 3</Menu.Item> */}
       </Menu>
+      ㅋㅋㅁㄴㅇㅁㄴㅇㅁㄴ
     </Header>
     <Layout>
       <Sider width={200} className="site-layout-background">
@@ -46,27 +31,17 @@ export default function First() {
           style={{ height: '100%', borderRight: 0 }}
         >
           <SubMenu key="sub1" icon={<UserOutlined />} title="보기 설정">
-              <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
-                모두 보기
-              </Checkbox>
-              <Divider />
-              <CheckboxGroup options={plainOptions} value={checkedList} onChange={onChange} />
-            {/* <Menu.Item key="1">내 일정</Menu.Item>
-            <Menu.Item key="2">휴가</Menu.Item>
-            <Menu.Item key="3">출장</Menu.Item>
-            <Menu.Item key="4">외근</Menu.Item> */}
+            <Menu.Item key="1"><Checkbox onChange={onChange}>내 일정</Checkbox></Menu.Item>
+            <Menu.Item key="2"><Checkbox onChange={onChange}>휴가</Checkbox></Menu.Item>
+            <Menu.Item key="3"><Checkbox onChange={onChange}>출장</Checkbox></Menu.Item>
+            <Menu.Item key="4"><Checkbox onChange={onChange}>외근</Checkbox></Menu.Item>
           </SubMenu>
           <SubMenu key="sub2" icon={<LaptopOutlined />} title="내 정보">
             <Menu.Item key="5">프로필 수정</Menu.Item>
-            {/* <Menu.Item key="6">option6</Menu.Item>
-            <Menu.Item key="7">option7</Menu.Item>
-            <Menu.Item key="8">option8</Menu.Item> */}
           </SubMenu>
           <SubMenu key="sub3" icon={<NotificationOutlined />} title="내 근무">
-            <Menu.Item key="9">휴가 등록</Menu.Item>
-            <Menu.Item key="10">근무 현황</Menu.Item>
-            {/* <Menu.Item key="11">option11</Menu.Item>
-            <Menu.Item key="12">option12</Menu.Item> */}
+            <Menu.Item key="6">휴가 등록</Menu.Item>
+            <Menu.Item key="7">근무 현황</Menu.Item>
           </SubMenu>
         </Menu>
       </Sider>
@@ -76,10 +51,11 @@ export default function First() {
           style={{
             padding: 24,
             margin: 0,
-            minHeight: 280,
+            height: 600
+            // minHeight: 280,
           }}
         >
-          내용
+          캘린더
         </Content>
       </Layout>
     </Layout>
