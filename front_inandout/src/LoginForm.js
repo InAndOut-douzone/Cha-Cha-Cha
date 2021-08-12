422775bef159582e082f4fb103780fdbb9d8a67e
jaes <dlwo9503@naver.com>
2021-08-12 14:12:38 +0900

    코드 정리 및 네이게이션 메뉴 추가

--- front_inandout/src/LoginForm.js
+++ front_inandout/src/LoginForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Form, Input } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
 
 
 const LoginForm = ({ onChange, fields }) => {
