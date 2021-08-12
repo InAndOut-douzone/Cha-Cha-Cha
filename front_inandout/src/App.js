8bf935cae078dd94d1d2acbc68a84a33447a2b59
jaes <dlwo9503@naver.com>
2021-08-12 23:07:10 +0900

    코드 정리

--- front_inandout/src/App.js
+++ front_inandout/src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { Layout } from 'antd';
-import Header from './components/_Header';
-import Navigation from './components/Navigation';
-import Footer from './components/_Footer';
+import { Route, Switch } from 'react-router-dom'
 import First from './First';
 import Login from './pages/Login';
 import MyPage from './pages/MyPage';
-import Leave from './pages/Leave';
 import Work from './pages/Work';
-import HIM from './pages/HIM';
-import WTM from './pages/WTM';
-import Add_Employee from './pages/Add_Employee';
-import Add_Notice from './pages/Add_Notice';
-import Employee_Management from './pages/Employee_Management';
-import Leave_Management from './pages/Leave_Management';
-import img from './assets/images/hospital.jpg';
 import PrivateRoute from './components/routes/PrivateRoute';
 import PublicRoute from './components/routes/PublicRoute';
 import LogoutRoute from './components/routes/LogoutRoute';
-import styled from 'styled-components'
 import PageNotFound from './pages/error/PageNotFound';
 
 export default function App() {
