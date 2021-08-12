db14f105fa328c121778c947ea29a75aa3f43218
jaes <dlwo9503@naver.com>
2021-08-12 15:39:54 +0900

    네이게이션 메뉴 추가

--- front_inandout/src/App.js
+++ front_inandout/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
+import { Layout } from 'antd';
+import Header from './components/_Header';
+import Navigation from './components/Navigation';
+import Footer from './components/_Footer';
 import First from './First';
 import Login from './Login';
 import MyPage from './pages/MyPage';
-import Leave from './pages/Leave'
-import Work from './pages/Work'
-import Header from './components/_Header';
-import Navigation from './components/Navigation';
-import Footer from './components/_Footer'
-import { Layout } from 'antd';
-import './assets/css/app.css';
-import styled from 'styled-components';
+import Leave from './pages/Leave';
+import Work from './pages/Work';
+import HIM from './pages/HIM';
+import WTM from './pages/WTM';
+import Add_Employee from './pages/Add_Employee';
+import Add_Notice from './pages/Add_Notice';
+import Employee_Management from './pages/Employee_Management';
+import Leave_Management from './pages/Leave_Management';
 import img from './assets/images/hospital.jpg';
+import styled from 'styled-components';
+import './assets/css/app.css';
 
 export default function App() {
   return (
@@ -26,6 +32,12 @@ export default function App() {
               <Route path="/mypage" exact={true} component={MyPage} />
               <Route path="/leave" exact={true} component={Leave} />
               <Route path="/work" exact={true} component={Work} />
+              <Route path="/him" exact={true} component={HIM} />
+              <Route path="/wtm" exact={true} component={WTM} />
+              <Route path="/addEmployee" exact={true} component={Add_Employee} />
+              <Route path="/addNotice" exact={true} component={Add_Notice} />
+              <Route path="/employeeManagement" exact={true} component={Employee_Management} />
+              <Route path="/leaveManagement" exact={true} component={Leave_Management} />
             </Layout>
             <Footer />
           </Layout> :
---
+++ front_inandout/src/pages/HIM.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HIM = () => {
+    return (
+        <div>
+            [병원 정보 관리 화면]
+        </div>
+    );
+};
+
+export default HIM;
