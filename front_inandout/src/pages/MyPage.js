2b56ac264ea6ed89f9d9d750aff338ad6a00955d
ruby723 <molly723819@gmail.com>
2021-08-18 11:45:51 +0900

    mypage useEffect

--- front_inandout/src/pages/MyPage.js
+++ front_inandout/src/pages/MyPage.js
@@ -1,25 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Layout, Descriptions, Badge, Breadcrumb, Input,Form,Button } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined } from '@ant-design/icons';
 
 const MyPage = () => {
-
+    
     const [user,setUser] = useState({});
 
-    const data = axios.get("http://localhost:8080/api/user/1").then((res)=>{
-        console.log(123,res);
-        console.log(12344,res.data.name);
-        setUser(res.data);
+    useEffect(() => {
+        const data = axios.get("http://localhost:8080/api/user/1").then((res)=>{
+            console.log(123,res);
+            console.log(12344,res.data.name);
+            setUser(res.data);
+        });
+        
+        console.log(data.data);
     });
-
-    // useEffect 써서 처음 한번만 실행되게 하기
-    
-    console.log(data.data);
-    
-
- 
 
     return (
         <Layout style={{ padding: '0 24px 24px' }}>
@@ -32,18 +29,18 @@ const MyPage = () => {
             <Descriptions title="사용자 정보" layout="vertical" bordered>
                 <Descriptions.Item label="이름">{user.name}</Descriptions.Item>
                 <Descriptions.Item label="직급">{user.position}</Descriptions.Item>
-                <Descriptions.Item label="생년월일">바꿀 수 없음</Descriptions.Item>
+                <Descriptions.Item label="생년월일">{user.birth}</Descriptions.Item>
                 
                 <Descriptions.Item label="이메일">
                     <Form.Item name="email" 
                         rules={[{type:'email', message:'이메일형식을 맞게 입력하세요.'}]}>
-                        <Input style={{width:'350px'}}/>
+                        <Input value={user.email} style={{width:'350px'}} />
                     </Form.Item>
                 </Descriptions.Item>
 
                 <Descriptions.Item label="연락처" span={2}>
                     <Form.Item rules={[{ required:true, message:'연락처를 입력하세요.'}]}>
-                        <Input />
+                        <Input value={user.phone} />
                     </Form.Item>
                 </Descriptions.Item>
                 
