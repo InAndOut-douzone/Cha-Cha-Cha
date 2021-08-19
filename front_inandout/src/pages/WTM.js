3c02195b37d67a9f68028b55f9009cab284f3ed7
jeonghyun051 <rlawjdgus051@naver.com>
2021-08-19 15:14:56 +0900

    wtm 진행중

--- front_inandout/src/pages/WTM.js
+++ front_inandout/src/pages/WTM.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { Layout, Breadcrumb } from 'antd';
+import { Layout, Breadcrumb, Form, DatePicker, TimePicker, Button } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined } from '@ant-design/icons';
 
+  const config = {
+    rules: [
+      {
+        type: 'object',
+        required: true,
+        message: 'Please select time!',
+      },
+    ],
+  };
+
 const WTM = () => {
+    const onFinish = (value) => {
+       console.log("value:" + value);
+      };
     return (
         <Layout style={{ padding: '0 24px 24px' }}>
             <br />
@@ -14,8 +27,32 @@ const WTM = () => {
             </Breadcrumb>
             <div style={{ borderTop: "1px solid #eee" }} />
             <br /><br />
-            [근무시간 관리 화면]
-        </Layout>
+            <strong>근무시간 등록</strong>
+            <Form name="time_related_controls" onFinish={onFinish}>
+                <Form.Item id="f" name="time-picker" label="TimePicker" {...config}>
+                    <TimePicker />
+                </Form.Item>
+                <Form.Item name="time-picker2" label="TimePicker" {...config}>
+                    <TimePicker />
+                </Form.Item>
+                <Form.Item
+                    wrapperCol={{
+                    xs: {
+                        span: 24,
+                        offset: 0,
+                    },
+                    sm: {
+                        span: 16,
+                        offset: 8,
+                    },
+                    }}
+                >
+                    <Button type="primary" htmlType="submit">
+                    Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+         </Layout>
     );
 };
 
