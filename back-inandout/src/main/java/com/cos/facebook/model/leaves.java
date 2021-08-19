6f6b21f164e75a23860c638bb41968a4a4f33689
jeonghyun051 <rlawjdgus051@naver.com>
2021-08-19 11:26:07 +0900

    entity add

--- back-inandout/src/main/java/com/cos/facebook/model/leaves.java
+++ back-inandout/src/main/java/com/cos/facebook/model/leaves.java
@@ -1,5 +1,28 @@
 package com.cos.facebook.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Data
+@Entity
 public class leaves {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //번호증가 전략 3가지 Table, auto_increment, Sequence / 기본 전략을 따르겠다
+	private int no;
+	private Date fromDate; // 휴가 시작일
+	private Date toDate;	  // 휴가 마감일
+	private String state;
+	
+	@ManyToOne
+	@JoinColumn(name="userId")
+	private User user;
 }
