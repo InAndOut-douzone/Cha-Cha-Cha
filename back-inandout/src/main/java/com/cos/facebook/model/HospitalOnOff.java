b2ba94fcac6b74bd9afd0fb8fb5a2c33ab4cb9c9
jeonghyun051 <rlawjdgus051@naver.com>
2021-08-20 17:40:00 +0900

    wtm 진행중

--- back-inandout/src/main/java/com/cos/facebook/model/HospitalOnOff.java
+++ back-inandout/src/main/java/com/cos/facebook/model/HospitalOnOff.java
@@ -1,25 +1,27 @@
 package com.cos.facebook.model;
 
-import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 import lombok.Data;
 
 @Data
 @Entity
-public class Hospital {
-    
+public class HospitalOnOff {
+ 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int no;
-	private String name;
-	private String address;
-	private String telNum;
-	private String logo;
-	private String ceoName;
+	private String week;
+	private String onTime;
+	private String offTime;
 	
+	@ManyToOne
+	@JoinColumn(name="hospitalNo")
+	private Hospital hospital;
 }
