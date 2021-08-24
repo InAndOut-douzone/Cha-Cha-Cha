e164287c4d54e0df89fb9ae79d15b5046fcf19af
jeonghyun051 <rlawjdgus051@naver.com>
2021-08-24 11:58:54 +0900

    role 설정

---
+++ back-inandout/src/main/java/com/cos/facebook/dto/UserReqRoleUpdateDto.java
@@ -0,0 +1,9 @@
+package com.cos.facebook.dto;
+
+import lombok.Data;
+
+@Data
+public class UserReqRoleUpdateDto {
+	
+	private String roles;
+}
--- front_inandout/src/pages/user/Employee_Details.js
+++ front_inandout/src/pages/user/Employee_Details.js
@@ -1,27 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Layout, Descriptions, Table, Breadcrumb } from 'antd';
+import { Layout, Descriptions, Table, Breadcrumb, Select, Form } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined } from '@ant-design/icons';
 import axios from 'axios';
 import SiteLayout from '../SiteLayout';
 import moment from 'moment';
 
-const dataSource = [
-    {
-        key: '1',
-        date: '0000-00-00',
-        on_time: '0000-00-00 00:00:00',
-        off_time: '0000-00-00 00:00:00',
-        state: '연차',
-    },
-    {
-        key: '2',
-        date: '0000-00-00',
-        on_time: '0000-00-00 00:00:00',
-        off_time: '0000-00-00 00:00:00',
-        state: '반차',
-    },
-];
+const { Option } = Select;
 
 const columns = [
 
@@ -87,9 +72,16 @@ const Employee_Details = (props) => {
         onTime: moment(work.onTime).format("HH : mm"),
         offTime: moment(work.offTime).format("HH : mm") !== "Invalid date" ? moment(work.offTime).format("HH : mm") : "",
         state: work.state
-    }))
+    }));
 
-    console.log("data:" + data);
+    const asd = (value) => {
+        let data = {
+            roles : value
+        }
+        axios.put("http://localhost:8080/api/user/"+id,data, header).then( res => {
+           
+        }).catch();
+    }
     
     return (
         <SiteLayout>
@@ -104,7 +96,7 @@ const Employee_Details = (props) => {
             <div style={{ borderTop: "1px solid #eee" }}/>
             <br /><br />
             <Link to="/employeemanagement" style={{textAlign:"end"}}>사원 목록 이동</Link>
-            <Descriptions title="[사원 이름]" bordered>
+            <Descriptions title="[사원 정보]" bordered>
                 <Descriptions.Item label="이름">{user.name}</Descriptions.Item>
                 <Descriptions.Item label="생년월일">{user.birth}</Descriptions.Item>
                 <Descriptions.Item label="성별">{user.gender}</Descriptions.Item>
@@ -117,6 +109,22 @@ const Employee_Details = (props) => {
                 <Descriptions.Item label="월차갯수">{user.mLeave}</Descriptions.Item>
                 <Descriptions.Item label="연차갯수">{user.aLeave}</Descriptions.Item>
                 <Descriptions.Item label="반차갯수"></Descriptions.Item>
+                <Form.Item
+                    name="roles"
+                    label="권한"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your roles!',
+                            whitespace: true,
+                        },
+                    ]}
+                >
+                    <Select onChange={asd} placeholder="권한을 설정해주세요. ">
+                        <Option value="ROLE_USER">일반</Option>
+                        <Option value="ROLE_ADMIN">관리자</Option>
+                    </Select> &nbsp; * 자동 저장 됩니다.
+                </Form.Item>
             </Descriptions>
             <br /><br/>
             [근무 현황]
