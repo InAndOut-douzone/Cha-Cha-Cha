a543501674ea1f4eedf9099b96862cae2f0d80aa
jeonghyun051 <rlawjdgus051@naver.com>
2021-08-26 15:35:27 +0900

    onofflist

--- front_inandout/src/pages/user/EmployeeOnOffList.js
+++ front_inandout/src/pages/user/EmployeeOnOffList.js
@@ -46,7 +46,7 @@ const EmployeeOnOffList = () => {
                         />
                     </List.Item>
                     )}
-                />,
+                />
             </div>
         </EmployeeOnOffListLayout>
     );
