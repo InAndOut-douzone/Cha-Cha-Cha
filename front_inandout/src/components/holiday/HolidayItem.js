57a2d68d03f12b081d6a95d42a5c846e0ca06958
jeonghyun051 <rlawjdgus051@naver.com>
2021-09-07 11:42:58 +0900

    wtm

--- front_inandout/src/components/holiday/HolidayItem.js
+++ front_inandout/src/components/holiday/HolidayItem.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import moment from 'moment';
+import { Timeline } from 'antd';
 
 const HolidayItem = (props) => {
     const { content, holiday } = props.holiday;
     
+    const randomColor = () => {
+        return "#" + Math.floor(Math.random() * 16777215).toString(16);
+      };
+
     return (
-        <div>
-            {moment(holiday).format("yyyy MM")} : {content}
-        </div>
+        <Timeline.Item style={{color:randomColor()}}>{moment(holiday).format("yyyy MM DD")} : {content}</Timeline.Item>
     );
 };
 
