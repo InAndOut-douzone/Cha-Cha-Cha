94314a8ba4870310fe5a34b22a674d31d84dd00b
ruby723 <molly723819@gmail.com>
2021-09-07 17:13:36 +0900

    월차, 연차 자동생성

--- back-inandout/src/main/java/com/cos/facebook/batch/DayTasklet.java
+++ back-inandout/src/main/java/com/cos/facebook/batch/DayTasklet.java
@@ -1,5 +1,6 @@
 package com.cos.facebook.batch;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -15,9 +16,11 @@ import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.cos.facebook.model.HospitalOnOff;
 import com.cos.facebook.model.OnOff;
 import com.cos.facebook.model.User;
 import com.cos.facebook.repository.HolidayRepository;
+import com.cos.facebook.repository.HospitalOnOffRepository;
 import com.cos.facebook.repository.OnOffRepository;
 import com.cos.facebook.repository.UserRepository;
 
@@ -32,20 +35,23 @@ public class DayTasklet implements Tasklet, StepExecutionListener {
 	@Autowired
 	private HolidayRepository holidayRepository;
 	
+	@Autowired
+	private HospitalOnOffRepository hospitalOnOffRepository;
+	
 	List<User> users = new ArrayList<User>();
 	List<Long> ids = new ArrayList<Long>();
 	String today = null;
 	
 	@Override
-	public void beforeStep(StepExecution stepExecution) {
+	public void beforeStep(StepExecution stepExecution) { // before listener
 		
-		users = userRepository.findAll();
-		for(User user : users) {
+		users = userRepository.findAllLive();
+		for(User user : users) {  // user id만 리스트로 뽑아둔다.
 			ids.add(user.getId());
 		}
 		
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		today = LocalDateTime.now().format(formatter);
+		today = LocalDateTime.now().format(formatter); // 오늘날짜 string으로 변
 
 	}
 
@@ -58,13 +64,31 @@ public class DayTasklet implements Tasklet, StepExecutionListener {
 	@Override
 	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 		
-		Boolean holiday = holidayRepository.findByDate(today) == null ? false : true;
+		Boolean holiday = holidayRepository.findByDate(today) == null ? false : true; // 오늘이 병원지정 휴일이면 true
 		
-		for(Long id : ids) {
+		SimpleDateFormat week = new SimpleDateFormat("E");  // 오늘 요일 받아오기
+		String strWeek = week.format(new Date());
+		if (strWeek.equals("목")) { strWeek = "Thu"; } 
+		if (strWeek.equals("월")) { strWeek = "Mon"; }
+		if (strWeek.equals("화")) { strWeek = "Tue"; }
+		if (strWeek.equals("수")) { strWeek = "Wed"; }
+		if (strWeek.equals("금")) { strWeek = "Fri"; }
+		if (strWeek.equals("토")) { strWeek = "Sat"; }
+		if (strWeek.equals("일")) { strWeek = "Sun"; }
+		
+		HospitalOnOff hospitalHoliday = hospitalOnOffRepository.findByWeek(strWeek);
+		
+		if( hospitalHoliday.getOnTime() == hospitalHoliday.getOffTime()) { // 오늘이 병원 휴무일이면 holiday = false
+			holiday = true;
+		}
+		
+		for(Long id : ids) { // 각 사원마다 for문 실
 			// work = onOffRepository.findByIdAndDate(id, today);
 			OnOff work = new OnOff();
 			
-			if(onOffRepository.findByIdAndDate(id, today) == null && !holiday) {
+			if((onOffRepository.findByIdAndDate(id, today) == null
+					|| (onOffRepository.findByIdAndDate(id, today).getOffTime() == null && onOffRepository.findByIdAndDate(id, today).getState() == null))
+					&& !holiday) { // 병원지정 휴일이 아니고, 사원의 근무기록이 없을때 결근처리
 				work.setState("결근");
 				work.setUser(userRepository.findById2(id));
 				work.setDate(new Date());
