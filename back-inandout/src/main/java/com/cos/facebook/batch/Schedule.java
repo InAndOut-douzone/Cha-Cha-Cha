c256dab7c811faff08e6e4d7aae27cf4bb57d9f9
ruby723 <molly723819@gmail.com>
2021-09-07 09:57:54 +0900

    spring batch test

--- back-inandout/src/main/java/com/cos/facebook/batch/Schedule.java
+++ back-inandout/src/main/java/com/cos/facebook/batch/Schedule.java
@@ -1,9 +1,21 @@
 package com.cos.facebook.batch;
 
-import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.cos.facebook.config.BatchConfig;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,10 +23,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class Schedule {
 
+	@Autowired
+    private JobLauncher jobLauncher;
+	
+	@Autowired
+	private final BatchConfig batch;
+	
     // 5초마다 실행
     @Scheduled(cron="00 * * * * *")
     public void executeJob () {
-        System.out.println(LocalDateTime.now());
+    	
+    	 Map<String, JobParameter> confMap = new HashMap<>();
+         confMap.put("time", new JobParameter(System.currentTimeMillis()));
+         JobParameters jobParameters = new JobParameters(confMap);
+
+    	try {
+			jobLauncher.run(batch.job(), jobParameters);
+			System.out.println("*******");
+			
+		} catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException
+				| JobParametersInvalidException e) {
+			e.printStackTrace();
+		}
+  
     }
 
 }
